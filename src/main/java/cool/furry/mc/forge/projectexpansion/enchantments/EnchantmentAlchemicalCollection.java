de77bd197193a235fc7ad976705c1ac4ccb6e3ea
Donovan Daniels <hewwo@yiff.rocks>
2024-09-29 10:58:27 -0500

    Make enchantment only apply to books More often than not people get the enchantment when they don't want it, now it can only be gotten on books then applied to items

--- src/main/java/cool/furry/mc/forge/projectexpansion/enchantments/EnchantmentAlchemicalCollection.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/enchantments/EnchantmentAlchemicalCollection.java
@@ -1,6 +1,8 @@
 package cool.furry.mc.forge.projectexpansion.enchantments;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
@@ -17,5 +19,14 @@ public class EnchantmentAlchemicalCollection extends Enchantment  {
     @Override
     public int getMaxCost(int level) {
         return getMinCost(level) + 50;
+    }
+
+    @Override
+    public boolean canEnchant(ItemStack stack) {
+        return category.canEnchant(stack.getItem());
+    }
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack) {
+        return stack.is(Items.BOOK);
     }
 }
