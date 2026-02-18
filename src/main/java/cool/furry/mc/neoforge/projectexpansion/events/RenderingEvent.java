1e17a645711e96f698bd0e7a8c0f7eddb08d507d
Donovan_DMC <hewwo@yiff.rocks>
2026-02-17 18:28:41 -0600

    Fix chest rendering

--- src/main/java/cool/furry/mc/neoforge/projectexpansion/events/RenderingEvent.java
+++ src/main/java/cool/furry/mc/neoforge/projectexpansion/events/RenderingEvent.java
@@ -1,28 +1,25 @@
 package cool.furry.mc.neoforge.projectexpansion.events;
 
 import cool.furry.mc.neoforge.projectexpansion.Main;
-import cool.furry.mc.neoforge.projectexpansion.block.BlockAdvancedAlchemicalChest;
 import cool.furry.mc.neoforge.projectexpansion.registries.BlockEntityTypes;
-import cool.furry.mc.neoforge.projectexpansion.registries.Blocks;
 import cool.furry.mc.neoforge.projectexpansion.rendering.ChestRenderer;
-import cool.furry.mc.neoforge.projectexpansion.util.AdvancedAlchemicalChest;
-import moze_intel.projecte.PECore;
-import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.level.block.Block;
+import cool.furry.mc.neoforge.projectexpansion.util.IChestLike;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
-import net.neoforged.neoforge.registries.DeferredHolder;
 
 @EventBusSubscriber(modid = Main.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class RenderingEvent {
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        for(DyeColor color: DyeColor.values()) {
-            DeferredHolder<Block, BlockAdvancedAlchemicalChest> block = AdvancedAlchemicalChest.getRegistryBlock(color);
-            event.registerBlockEntityRenderer(BlockEntityTypes.ADVANCED_ALCHEMICAL_CHEST.get(), context -> new ChestRenderer<>(context, Main.rl(String.format("textures/block/advanced_alchemical_chest/%s.png", color.getName())), block));
-        }
-        event.registerBlockEntityRenderer(BlockEntityTypes.CONDENSER_MK3.get(), context -> new ChestRenderer<>(context, PECore.rl("textures/block/condenser_mk1.png"), Blocks.CONDENSER_MK3));
+        registerChest(event, BlockEntityTypes.ADVANCED_ALCHEMICAL_CHEST.get());
+        registerChest(event, BlockEntityTypes.CONDENSER_MK3.get());
+    }
+
+    private static <BE extends BlockEntity & IChestLike> void registerChest(EntityRenderersEvent.RegisterRenderers event, BlockEntityType<BE> type) {
+        event.registerBlockEntityRenderer(type, context -> new ChestRenderer<>(context, type));
     }
 }
--- src/main/java/cool/furry/mc/neoforge/projectexpansion/rendering/ChestRenderer.java
+++ src/main/java/cool/furry/mc/neoforge/projectexpansion/rendering/ChestRenderer.java
@@ -3,7 +3,11 @@ package cool.furry.mc.neoforge.projectexpansion.rendering;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
+import cool.furry.mc.neoforge.projectexpansion.Main;
+import cool.furry.mc.neoforge.projectexpansion.block.BlockAdvancedAlchemicalChest;
+import cool.furry.mc.neoforge.projectexpansion.block.BlockCondenserMK3;
 import cool.furry.mc.neoforge.projectexpansion.util.IChestLike;
+import moze_intel.projecte.PECore;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -13,25 +17,23 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.neoforged.neoforge.registries.DeferredHolder;
 
-import java.util.function.Predicate;
+import javax.annotation.Nullable;
 
 // lovingly "lifted" from ProjectE
 // https://github.com/sinkillerj/ProjectE/blob/98aee771bdb09beecf51b5608938d93de6f1afb6/src/main/java/moze_intel/projecte/rendering/ChestRenderer.java
-public class ChestRenderer <B extends Block, BE extends BlockEntity & IChestLike> implements BlockEntityRenderer<BE> {
+public class ChestRenderer <BE extends BlockEntity & IChestLike> implements BlockEntityRenderer<BE> {
 	private final ModelPart lid;
 	private final ModelPart bottom;
 	private final ModelPart lock;
 
-	private final Predicate<Block> blockChecker;
-	private final ResourceLocation texture;
+	private final BlockEntityType<BE> type;
 
-	public ChestRenderer(BlockEntityRendererProvider.Context context, ResourceLocation texture, DeferredHolder<Block, B> type) {
-		this.texture = texture;
-		this.blockChecker = block -> block == type.get();
+	public ChestRenderer(BlockEntityRendererProvider.Context context, BlockEntityType<BE> type) {
+		this.type = type;
 		ModelPart modelpart = context.bakeLayer(ModelLayers.CHEST);
 		this.bottom = modelpart.getChild("bottom");
 		this.lid = modelpart.getChild("lid");
@@ -39,24 +41,33 @@ public class ChestRenderer <B extends Block, BE extends BlockEntity & IChestLike
 	}
 
 	@Override
-	public void render(BE chest, float partialTick, PoseStack matrix, MultiBufferSource renderer, int light, int overlayLight) {
-		matrix.pushPose();
-		if (chest.getLevel() != null && !chest.isRemoved()) {
-			BlockState state = chest.getLevel().getBlockState(chest.getBlockPos());
-			if (blockChecker.test(state.getBlock())) {
-				matrix.translate(0.5D, 0.5D, 0.5D);
-				matrix.mulPose(Axis.YP.rotationDegrees(-state.getValue(BlockStateProperties.HORIZONTAL_FACING).toYRot()));
-				matrix.translate(-0.5D, -0.5D, -0.5D);
-			}
+	public void render(BlockEntity blockEntity, float partialTick, PoseStack matrix, MultiBufferSource renderer, int light, int overlayLight) {
+		if (blockEntity.getLevel() == null || blockEntity.isRemoved()) return;
+		BlockState state = blockEntity.getLevel().getBlockState(blockEntity.getBlockPos());
+		Block block = state.getBlock();
+		ResourceLocation texture = getTexture(block);
+		if (blockEntity.getType().equals(this.type) && blockEntity instanceof IChestLike chest && texture != null) {
+			matrix.pushPose();
+			matrix.translate(0.5D, 0.5D, 0.5D);
+			matrix.mulPose(Axis.YP.rotationDegrees(-state.getValue(BlockStateProperties.HORIZONTAL_FACING).toYRot()));
+			matrix.translate(-0.5D, -0.5D, -0.5D);
+			float lidAngle = 1.0F - chest.getOpenNess(partialTick);
+			lidAngle = 1.0F - lidAngle * lidAngle * lidAngle;
+			VertexConsumer builder = renderer.getBuffer(RenderType.entityCutout(texture));
+			lid.xRot = -(lidAngle * ((float) Math.PI / 2F));
+			lock.xRot = lid.xRot;
+			lid.render(matrix, builder, light, overlayLight);
+			lock.render(matrix, builder, light, overlayLight);
+			bottom.render(matrix, builder, light, overlayLight);
+			matrix.popPose();
 		}
-		float lidAngle = 1.0F - chest.getOpenNess(partialTick);
-		lidAngle = 1.0F - lidAngle * lidAngle * lidAngle;
-		VertexConsumer builder = renderer.getBuffer(RenderType.entityCutout(texture));
-		lid.xRot = -(lidAngle * ((float) Math.PI / 2F));
-		lock.xRot = lid.xRot;
-		lid.render(matrix, builder, light, overlayLight);
-		lock.render(matrix, builder, light, overlayLight);
-		bottom.render(matrix, builder, light, overlayLight);
-		matrix.popPose();
+	}
+
+	private @Nullable ResourceLocation getTexture(Block block) {
+		return switch (block) {
+			case BlockAdvancedAlchemicalChest chest -> Main.rl(String.format("textures/block/advanced_alchemical_chest/%s.png", chest.getColor().getName()));
+			case BlockCondenserMK3 ignored -> PECore.rl("textures/block/condenser_mk1.png");
+			default -> null;
+		};
 	}
 }
