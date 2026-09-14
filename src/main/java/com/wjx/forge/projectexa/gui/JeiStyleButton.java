5c18c6dc0ff57d0c91ff038f52615e82940cb8c3
WJXhhh <WJXhhh@users.noreply.github.com>
2026-09-15 07:55:03 +0800

    Add runtime mod dependencies and update transfer button icon

--- src/main/java/com/wjx/forge/projectexa/gui/JeiStyleButton.java
+++ src/main/java/com/wjx/forge/projectexa/gui/JeiStyleButton.java
@@ -5,7 +5,7 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
-/** A small JEI-looking button that uses JEI's own background and recipe-transfer icon. */
+/** A small JEI-looking button with the ProjectExA transfer icon. */
 public class JeiStyleButton extends Button {
     private static final ResourceLocation ENABLED_TEXTURE =
             new ResourceLocation("jei", "textures/jei/atlas/gui/button_enabled.png");
@@ -13,8 +13,8 @@ public class JeiStyleButton extends Button {
             new ResourceLocation("jei", "textures/jei/atlas/gui/button_highlight.png");
     private static final ResourceLocation DISABLED_TEXTURE =
             new ResourceLocation("jei", "textures/jei/atlas/gui/button_disabled.png");
-    private static final ResourceLocation RECIPE_TRANSFER_ICON =
-            new ResourceLocation("jei", "textures/jei/atlas/gui/icons/recipe_transfer.png");
+    private static final ResourceLocation TRANSFER_ICON =
+            new ResourceLocation("projectexa", "textures/gui/arcanum_transfer.png");
 
     public JeiStyleButton(int x, int y, Component narration, OnPress onPress) {
         super(Button.builder(narration, onPress).pos(x, y).size(20, 20));
@@ -26,6 +26,6 @@ public class JeiStyleButton extends Button {
                 ? DISABLED_TEXTURE
                 : isHoveredOrFocused() ? HIGHLIGHT_TEXTURE : ENABLED_TEXTURE;
         graphics.blit(background, getX(), getY(), 0, 0, getWidth(), getHeight(), 20, 20);
-        graphics.blit(RECIPE_TRANSFER_ICON, getX() + 6, getY() + 6, 0, 0, 7, 7, 7, 7);
+        graphics.blit(TRANSFER_ICON, getX() + 3, getY() + 3, 0, 0, 14, 14, 14, 14);
     }
 }
